bbb28bc987a9896840e90a8e9b248499f5a3947a
ishikajain04 <85635648+ishikajain04@users.noreply.github.com>
2021-07-21 17:53:45 +0530

    Create operations_list.py

---
+++ operations_list.py
@@ -0,0 +1,7 @@
+/* OPERATION */
+
+
+operations = {'ADD': add ,'ADDITION':add,'PLUS':add,'SUM':add
+'SUB':sub,'SUBTRACTION':sub,'MINUS':sub,'DIFFERENCE':sub,'LCM':lcm,
+'HCF':hcf,'MUL':mul,'MULTIPLICATION':mul,'PRODUCT':mul,'MULTIPLY':mul,
+'DIVIDE':div,'DIV':div,'DIVISION':div,'MOD':mod,'MODULUS':mod,'REMAINDAR':mod}
